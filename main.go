548b2da61da75963a311c0742a444b133f2024bc
Roch D'Amour <roch.damour@gmail.com>
2023-09-14 00:53:53 -0400

    Added TTS capabilities

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/notarock/sludger/pkg/audio"
 	"github.com/notarock/sludger/pkg/reddit"
 )
 
@@ -28,4 +29,33 @@ func main() {
 
 	fmt.Printf("%+v\n", thread)
 	fmt.Println(thread.TotalComments())
+
+	if thread.Title == "" {
+		fmt.Println("No title found ??")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Processing audio...")
+
+	audio.GetVoiceFile(thread.Title, "audio/title.mp3")
+
+	files := []string{}
+	for _, value := range thread.CommentThreads {
+		if len(files) > 10 {
+			break
+		}
+		for _, comment := range value.Comments {
+			if len(files) > 10 {
+				break
+			}
+
+			filename := fmt.Sprintf("audio/%d.mp3", len(files))
+			audio.GetVoiceFile(comment, fmt.Sprintf("audio/%d.mp3", len(files)))
+			files = append(files, filename)
+		}
+	}
+
+	fmt.Printf("Processing audio...")
+	audio.Concatenate("audio/title.mp3", files, "output.mp3")
+
 }
