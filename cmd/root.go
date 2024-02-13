1eab15679393a537f43464c1fad7219bdb4c28cd
Roch D'Amour <roch.damour@gmail.com>
2024-02-12 21:59:29 -0500

    Version command

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,5 @@
 package cmd
+
 import (
 	"fmt"
 	"os"
@@ -8,18 +9,18 @@ import (
 
 
 var rootCmd = &cobra.Command{
-  Use:   "slopify [old.reddit.com URL]",
-  Short: "Slopify is a short-form content generator",
-  Long: `Slopify is a short-form content generator that takes a reddit post and
-  generates a short-form video with the post's content.`,
-  Run: func(cmd *cobra.Command, args []string) {
-	  full()
-  },
+	Use:   "slopify [old.reddit.com URL]",
+	Short: "Slopify is a short-form content generator",
+	Long: `Slopify is a short-form content generator that takes a reddit post and
+	generates a short-form video with the post's content.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		full()
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
