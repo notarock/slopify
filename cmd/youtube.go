90be5b286c26b8b2cf215f44183d6ae440a7a181
Roch D'Amour <roch.damour@gmail.com>
2024-02-26 01:37:56 -0500

    Work from a workspace

--- cmd/youtube.go
+++ cmd/youtube.go
@@ -9,7 +9,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var tokenFile string
+var oauthConfigFile string
+
+var videoPath string
+var title string
+var description string
+var privacyStatus string
+
 func init() {
+	youtubeCmd.PersistentFlags().StringVar(&tokenFile, "tokenFile", "token.json", "Path to your token file where you want to store your YouTube token. If not provided, the default token file will be used.")
+	youtubeCmd.PersistentFlags().StringVar(&oauthConfigFile, "oauthConfigFile", "youtubeConfig.json", "Path to your 0AUTH client secret file.")
+
+	youtubeCmd.PersistentFlags().StringVar(&videoPath, "videoPath", "", "Path to the video you want to upload")
+	youtubeCmd.PersistentFlags().StringVar(&title, "title", "My Uploaded Video", "Title of the video")
+	youtubeCmd.PersistentFlags().StringVar(&description, "description", "Description of my video", "Description of the video")
+	youtubeCmd.PersistentFlags().StringVar(&privacyStatus, "privacyStatus", "private", "Privacy status of the video")
+
 	rootCmd.AddCommand(youtubeCmd)
 }
 
@@ -25,35 +41,34 @@ var youtubeCmd = &cobra.Command{
 }
 
 func doYoutube(cfg config.Config, args []string) error {
-	clientSecretFile := "youtubeConfig.json" // Path to your client secret file
-	tokenFile := "token.json"                // Path to your token file
-
-	uploader, err := google.NewYouTubeUploader(clientSecretFile, tokenFile)
+	uploader, err := google.NewYouTubeUploader(oauthConfigFile, tokenFile)
 	if err != nil {
 		return fmt.Errorf("Error creating YouTube uploader: %v", err)
 	}
 
 	response, err := uploader.GetUserInfo()
 	if err != nil {
-		return fmt.Errorf("Error creating YouTube uploader: %v", err)
+		return fmt.Errorf("Error getting user info: %v", err)
 	}
+
 	for _, item := range response.Items {
-		fmt.Printf("Channel ID: %s\n", item.Id)
-		fmt.Printf("Title: %s\n", item.Snippet.Title)
-		fmt.Printf("Description: %s\n", item.Snippet.Description)
-		fmt.Printf("Custom URL: %s\n", item.Snippet.CustomUrl)
+		log.Printf("This is the info for your YouTube account: \n")
+		log.Printf("Channel ID: %s\n", item.Id)
+		log.Printf("Title: %s\n", item.Snippet.Title)
+		log.Printf("Description: %s\n", item.Snippet.Description)
+		log.Printf("Custom URL: %s\n", item.Snippet.CustomUrl)
 	}
 
-	videoPath := "slop-1708318402-subs.mp4" // Path to the video you want to upload
-	title := "My Uploaded Video"
-	description := "Description of my video"
-	privacyStatus := "private" // or "private" or "unlisted"
+	if videoPath == "" {
+		return fmt.Errorf("No video path provided. Please provide a valid video file path.")
+	}
 
+	log.Printf("Uploading video: %s\n", videoPath)
 	video, err := uploader.UploadVideo(videoPath, title, description, privacyStatus)
 	if err != nil {
 		return fmt.Errorf("Error uploading video: %v", err)
 	}
 
-	fmt.Printf("Video uploaded successfully: %s\n", video.Id)
+	log.Printf("Video uploaded successfully: %s\n", video.Id)
 	return nil
 }
--- pkg/google/storage.go
+++ pkg/google/storage.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"cloud.google.com/go/storage"
 )
@@ -25,8 +26,11 @@ func UploadFile(bucket, object string) (string, error) {
 	}
 	defer f.Close()
 
+	// Get the base name of the file (without directory path).
+	baseFilename := filepath.Base(object)
+
 	// Upload file to bucket.
-	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
+	wc := client.Bucket(bucket).Object(baseFilename).NewWriter(ctx)
 	if _, err = io.Copy(wc, f); err != nil {
 		return "", err
 	}
@@ -34,7 +38,7 @@ func UploadFile(bucket, object string) (string, error) {
 	if err := wc.Close(); err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("gs://%s/%s", bucket, object), nil
+	return fmt.Sprintf("gs://%s/%s", bucket, baseFilename), nil
 }
 
 // deleteFile deletes an object (file) from a bucket.
