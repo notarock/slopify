1db830fdbc1fb5f59da8d44e404d08ad717a92e7
Roch D'Amour <roch.damour@gmail.com>
2024-02-26 01:37:56 -0500

    Work from a workspace

--- cmd/reddit.go
+++ cmd/reddit.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/url"
+	"os"
 	"strconv"
 	"time"
 
@@ -17,36 +18,45 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-const SUBTITLE_COMMAND = "subtitles=output.srt:force_style='FontSize=24,Alignment=10'"
+const SUBTITLE_COMMAND = "subtitles=%s:force_style='FontSize=24,Alignment=10'"
 const BUCKET_NAME = "slopify-transcription-buffer"
 const SUBTITLES_FILE = "output.srt"
 
 var footageDir string
+var workingDir string
 
 func init() {
 	rootCmd.PersistentFlags().StringVar(&footageDir, "footage", "", "Directory to search for footage. If not provided, the default footage will be used.")
+
+	redditCmd.PersistentFlags().StringVar(&workingDir, "workingDir", "/tmp/slopify", "Directory to store temporary files. If not provided, the default working directory will be used.")
+
 	rootCmd.AddCommand(redditCmd)
 }
 
 var redditCmd = &cobra.Command{
 	Use:   "reddit https://old.reddit.com/r/...",
 	Short: "Generate a video from a reddit thread",
-	Long: `Generate a video from a reddit URL or a file containing the HTML of the thread.`,
+	Long:  `Generate a video from a reddit URL or a file containing the HTML of the thread.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if	err := redditVideo(cfg,args); err != nil {
+		if err := redditVideo(cfg, args); err != nil {
 			log.Fatalf("Error generating slop from reddit thread: %v", err)
 		}
 	},
 }
 
-func redditVideo(cfg config.Config, args []string) (error) {
+func redditVideo(cfg config.Config, args []string) error {
 	if len(args) < 1 {
 		return fmt.Errorf("No URL or file were provided. Please provide a URL or file containing the HTML of the thread.")
 	}
 
+	err := createWorkspace(workingDir)
+	if err != nil {
+		return fmt.Errorf("Error creating workspace: %v", err)
+	}
+
 	arg := args[0]
 
-	_, err := url.ParseRequestURI(arg)
+	_, err = url.ParseRequestURI(arg)
 
 	// If it's a URL, scrape it
 	// If it's a file, scrape it
@@ -58,18 +68,12 @@ func redditVideo(cfg config.Config, args []string) (error) {
 		thread = reddit.ScrapeFromFile(arg)
 	}
 
-	// TODO: Debug
-	// fmt.Printf("%+v\n", thread)
-	// fmt.Println(thread.TotalComments())
-
 	if thread.Title == "" {
 		return fmt.Errorf("No title found in the thread, please provide a valid URL or file containing the HTML of the thread.")
 	}
 
-	// TODO: Debug
-	// fmt.Printf("Processing audio...")
-
-	google.GetVoiceFile(thread.Title, "audio/title.mp3")
+	titlePath := workingDir + "/audio/title.mp3"
+	google.GetVoiceFile(thread.Title, titlePath)
 
 	// TODO: Function to get the top comments and limit depth
 	files := []string{}
@@ -82,13 +86,15 @@ func redditVideo(cfg config.Config, args []string) (error) {
 				break
 			}
 
-			filename := fmt.Sprintf("audio/%d.mp3", len(files))
-			google.GetVoiceFile(comment, fmt.Sprintf("audio/%d.mp3", len(files)))
+			filename := fmt.Sprintf(workingDir+"/"+"audio/%d.mp3", len(files))
+			google.GetVoiceFile(comment, filename)
 			files = append(files, filename)
 		}
 	}
 
-	google.Concatenate("audio/title.mp3", files, "output.mp3")
+	fullAudioPath := workingDir + "/output.mp3"
+
+	google.Concatenate("audio/title.mp3", files, fullAudioPath)
 
 	var videoFile string
 
@@ -102,8 +108,6 @@ func redditVideo(cfg config.Config, args []string) (error) {
 		videoFile = "source.webm"
 	}
 
-	audioFile := "output.mp3"
-
 	// TODO: Randomize the start time
 	// This works when using an hour long footage of subway surfer
 	//
@@ -113,9 +117,9 @@ func redditVideo(cfg config.Config, args []string) (error) {
 	// video := ffmpeg.Input(videoFile, ffmpeg.KwArgs{"ss": fmt.Sprintf("00:%d:55", randomNumber)}).Video()
 
 	video := ffmpeg.Input(videoFile).Video()
-	audio := ffmpeg.Input(audioFile).Audio()
+	audio := ffmpeg.Input(fullAudioPath).Audio()
 
-	outputFile := "slop-" + strconv.FormatInt(time.Now().Unix(), 10)
+	outputFile := workingDir + "/" + "slop-" + strconv.FormatInt(time.Now().Unix(), 10)
 	outputFileWithSubs := outputFile + "-subs.mp4"
 	outputFile = outputFile + ".mp4"
 
@@ -156,17 +160,19 @@ func redditVideo(cfg config.Config, args []string) (error) {
 	srtData := subs.ConvertToSRT(fullTranscript)
 
 	// Write to SRT file
-	err = subs.WriteSRT(SUBTITLES_FILE, srtData)
+	srtPath := workingDir + "/" + SUBTITLES_FILE
+	err = subs.WriteSRT(srtPath, srtData)
 	if err != nil {
 		return fmt.Errorf("Error writing SRT file: %v", err)
 	}
 
-	err = ffmpeg.Input(outputFile).Output(outputFileWithSubs, ffmpeg.KwArgs{"vf": SUBTITLE_COMMAND}).Run()
+	err = ffmpeg.Input(outputFile).Output(outputFileWithSubs, ffmpeg.KwArgs{"vf": fmt.Sprintf(SUBTITLE_COMMAND, srtPath)}).Run()
 	if err != nil {
 		return fmt.Errorf("Error adding subtitles to video: %v", err)
 	}
 
 	fmt.Println("All done! Your video is ready at " + outputFileWithSubs)
+	fmt.Println("output path: " + outputFileWithSubs)
 
 	return nil
 }
@@ -178,7 +184,39 @@ func pickFromDirectory(dir string) (string, error) {
 	}
 
 	randomWithSeed := rand.New(rand.NewSource(time.Now().UnixNano()))
-	randomNumber := randomWithSeed.Intn(len(files))    // Generate a number between 0 and 55 (inclusive)
+	randomNumber := randomWithSeed.Intn(len(files)) // Generate a number between 0 and 55 (inclusive)
 
 	return dir + "/" + files[randomNumber].Name(), nil
+}
+
+func createWorkspace(directory string) error {
+	err := createDirectory(directory)
+	if err != nil {
+		return fmt.Errorf("Error creating workspace: %v", err)
+	}
+	err = createDirectory(directory + "/audio")
+	if err != nil {
+		return fmt.Errorf("Error creating audio workspace: %v", err)
+	}
+	err = createDirectory(directory + "/video")
+	if err != nil {
+		return fmt.Errorf("Error creating video workspace: %v", err)
+	}
+	return nil
+}
+
+func createDirectory(directory string) error {
+	// Check if the directory exists
+	if _, err := os.Stat(directory); os.IsNotExist(err) {
+		// Directory does not exist, create it
+		err := os.Mkdir(directory, 0755) // 0755 is the default permission
+		if err != nil {
+			return fmt.Errorf("Error creating directory: %v", err)
+		}
+		log.Println("Directory", directory, "created successfully.")
+	} else if err != nil {
+		// Some error occurred while checking directory existence
+		return err
+	}
+	return nil
 }
