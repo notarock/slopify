c1730e7661bf8cc54c29201d2bd6b9949f6447b2
Roch D'Amour <roch.damour@gmail.com>
2024-03-07 01:25:44 -0500

    Integrated title+description+youtube upload

--- pkg/reddit/thread.go
+++ pkg/reddit/thread.go
@@ -15,6 +15,16 @@ type Comment struct {
 	Comments []string
 }
 
+func (t Thread) ToString() string {
+	output := t.Title + "\n"
+	for _, comment := range t.CommentThreads {
+		for _, c := range comment.Comments {
+			output += c + "\n"
+		}
+	}
+	return output
+}
+
 func (t Thread) TotalComments() int {
 	total := 0
 	for _, comment := range t.CommentThreads {
